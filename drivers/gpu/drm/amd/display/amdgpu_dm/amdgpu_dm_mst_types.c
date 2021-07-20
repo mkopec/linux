84cb51a618cbbe1e05f37536bada5325082c9691
Michał Kopeć <michal@nozomi.space>
2021-07-20 17:48:20 +0200

    More debug

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -410,8 +410,12 @@ dm_dp_add_mst_connector(struct drm_dp_mst_topology_mgr *mgr,
 		drm_connector_attach_max_bpc_property(connector, 8, 16);
 
 	connector->vrr_capable_property = master->base.vrr_capable_property;
-	if (connector->vrr_capable_property)
+	if (connector->vrr_capable_property) {
+		DRM_INFO("Attaching vrr_capable_property");
 		drm_connector_attach_vrr_capable_property(connector);
+	} else {
+		DRM_INFO("Not attaching vrr_capable_property");
+	}
 
 	drm_object_attach_property(
 		&connector->base,
