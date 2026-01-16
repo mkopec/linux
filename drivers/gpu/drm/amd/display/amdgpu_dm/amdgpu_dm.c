a7e11ca2aa266c686f1e7193ffc4f51c2a6b4f91
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-01-16 16:32:52 +0100

    drm/amd/display: Build HDMI vsif in correct slot
    [Why]
    HDMI vsif was assigned to vsp_infopacket (FreeSync) field
    
    [How]
    Build HDMI vsif in the correct hfvsif_infopacket field
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -7414,7 +7414,7 @@ create_stream_for_sink(struct drm_connector *connector,
 	update_stream_signal(stream, sink);
 
 	if (dc_is_hdmi_signal(stream->signal))
-		mod_build_hf_vsif_infopacket(stream, &stream->vsp_infopacket);
+		mod_build_hf_vsif_infopacket(stream, &stream->hfvsif_infopacket);
 
 	if (stream->signal == SIGNAL_TYPE_DISPLAY_PORT ||
 	    stream->signal == SIGNAL_TYPE_DISPLAY_PORT_MST ||
