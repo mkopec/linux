1a0458ce79d05aede390237f5ff7e530f25ac168
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-01-18 14:10:22 +0100

    drm/amd/display: Refactor PCON VRR compatibility check
    [Why]
    DP->HDMI PCONs prevously entered the DP path
    
    [How]
    Restructure amdgpu_dm_update_freesync_caps() and move
    dm_get_adaptive_sync_support_type() to dm_helpers_is_vrr_pcon_allowed()
    to better reflect what this function does. It never actually gave us any
    other info.
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -13341,8 +13341,8 @@ void amdgpu_dm_update_freesync_caps(struct drm_connector *connector,
 	struct dpcd_caps dpcd_caps = {0};
 	const struct edid *edid;
 	bool freesync_capable = false;
+	bool pcon_allowed = false;
 	bool is_pcon = false;
-	enum adaptive_sync_type as_type = ADAPTIVE_SYNC_TYPE_NONE;
 
 	if (!connector->state) {
 		drm_err(adev_to_drm(adev), "%s - Connector has no state", __func__);
@@ -13377,6 +13377,7 @@ void amdgpu_dm_update_freesync_caps(struct drm_connector *connector,
 	if (amdgpu_dm_connector->dc_link) {
 		dpcd_caps = amdgpu_dm_connector->dc_link->dpcd_caps;
 		is_pcon = dpcd_caps.dongle_type == DISPLAY_DONGLE_DP_HDMI_CONVERTER;
+		pcon_allowed = dm_helpers_is_vrr_pcon_allowed(amdgpu_dm_connector->dc_link);
 	}
 
 	/* DP & eDP excluding PCONs */
@@ -13410,15 +13411,11 @@ void amdgpu_dm_update_freesync_caps(struct drm_connector *connector,
 	} else if (dc_is_hdmi_signal(sink->sink_signal) && vsdb_info.freesync_supported) {
 		monitor_range_from_vsdb(&connector->display_info, &vsdb_info);
 		freesync_capable = copy_range_to_amdgpu_connector(connector);
-	}
-
-	if (amdgpu_dm_connector->dc_link && is_pcon)
-		as_type = dm_get_adaptive_sync_support_type(amdgpu_dm_connector->dc_link);
 
 	/* DP -> HDMI PCON */
-	if (as_type == FREESYNC_TYPE_PCON_IN_WHITELIST && vsdb_info.freesync_supported) {
+	} else if (pcon_allowed && vsdb_info.freesync_supported) {
+		amdgpu_dm_connector->as_type = FREESYNC_TYPE_PCON_IN_WHITELIST;
 		amdgpu_dm_connector->pack_sdp_v1_3 = true;
-		amdgpu_dm_connector->as_type = as_type;
 		amdgpu_dm_connector->vsdb_info = vsdb_info;
 
 		parse_amd_vsdb_cea(amdgpu_dm_connector, edid, &vsdb_info);
